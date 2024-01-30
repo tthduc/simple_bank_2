0f84128990e59a558e8637a648a6dc44157c9f8f
thanhduc <thanhduc736@gmail.com>
2024-01-30 09:48:41 +0700

    implemeted grpc gateway

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 	"log"
 	"net"
+	"net/http"
 	"simple_bank/api"
 	db "simple_bank/db/sqlc"
 	"simple_bank/gapi"
@@ -27,7 +31,7 @@ func main() {
 
 	store := db.NewStore(conn)
 
-	//runGinServer(config, store)
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 }
 
@@ -44,7 +48,7 @@ func runGrpcServer(config util.Config, store db.Store) {
 
 	listener, err := net.Listen("tcp", config.GRPCServerAddress)
 	if err != nil {
-		log.Fatal("cannot create listener")
+		log.Fatal("cannot create listener: ", err)
 	}
 
 	log.Printf("start gRPC server at %s", listener.Addr().String())
@@ -63,5 +67,54 @@ func runGinServer(config util.Config, store db.Store) {
 	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server")
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server")
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	//statikFS, err := fs.New()
+	//if err != nil {
+	//	log.Fatal().Err(err).Msg("cannot create statik fs")
+	//}
+
+	//swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	//mux.Handle("/swagger/", swaggerHandler)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+	//handler := gapi.HttpLogger(mux)
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway server")
 	}
 }
