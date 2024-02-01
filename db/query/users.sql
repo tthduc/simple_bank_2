278883b6124448a4354fe4f9e31d202c72726288
thanhduc <thanhduc736@gmail.com>
2024-02-01 11:25:14 +0700

    Build gRPC update API

--- db/query/users.sql
+++ db/query/users.sql
@@ -18,7 +18,8 @@ UPDATE users
 SET
     hashed_password = COALESCE(sqlc.narg(hashed_password), hashed_password),
     full_name = COALESCE(sqlc.narg(full_name), full_name),
-    email = COALESCE(sqlc.narg(email), email)
+    email = COALESCE(sqlc.narg(email), email),
+    password_changed_at = COALESCE(sqlc.narg(password_changed_at), password_changed_at)
 WHERE
     username = sqlc.arg(username)
 RETURNING *;
