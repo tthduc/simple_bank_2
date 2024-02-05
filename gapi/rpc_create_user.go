01aea44ef4b0c8d8471ba089b86c07a980ca54ac
thanhduc <thanhduc736@gmail.com>
2024-02-05 11:38:36 +0700

    Send async tasks to Redis within a DB transaction

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -25,41 +25,43 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
 	}
 
-	arg := db.CreateUserParams{
-		Username:       req.GetUsername(),
-		HashedPassword: hashedPassword,
-		FullName:       req.GetFullName(),
-		Email:          req.GetEmail(),
+	arg := db.CreateUserTxParams{
+		CreateUserParams: db.CreateUserParams{
+			Username:       req.GetUsername(),
+			HashedPassword: hashedPassword,
+			FullName:       req.GetFullName(),
+			Email:          req.GetEmail(),
+		},
+		AfterCreate: func(user db.User) error {
+			// Send verify email to user and user db transaction
+			taskPayload := &worker.PayloadSendVerifyEmail{
+				Username: user.Username,
+			}
+			opts := []asynq.Option{
+				// This means we only allow the tasks to  be retried at most 10 times if it fails.
+				asynq.MaxRetry(10),
+				//  it will only be picked up  by the processor after 10 seconds.
+				asynq.ProcessIn(10 * time.Second),
+				// if you have multiple tasks  with different priority levels, You can use the asynq.Queue() option  to send them to different queues.
+				asynq.Queue(worker.QueueCritical),
+			}
+
+			return server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
+		},
 	}
 
-	user, err := server.store.CreateUser(ctx, arg)
+	// if it fails to send the task, the whole transaction will be rolled back And the user won’t exist in the database,
+	// Which means, the client can safely retry the request without any issues.
+	txResult, err := server.store.CreateUserTx(ctx, arg)
 	if err != nil {
 		if db.ErrorCode(err) == db.UniqueViolation {
-			return nil, status.Errorf(codes.Internal, "username already exists: %s", err)
+			return nil, status.Errorf(codes.AlreadyExists, err.Error())
 		}
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
 	}
 
-	// Send verify email to user and user db transaction
-	taskPayload := &worker.PayloadSendVerifyEmail{
-		Username: user.Username,
-	}
-	opts := []asynq.Option{
-		// This means we only allow the tasks to  be retried at most 10 times if it fails.
-		asynq.MaxRetry(10),
-		//  it will only be picked up  by the processor after 10 seconds.
-		asynq.ProcessIn(10 * time.Second),
-		// if you have multiple tasks  with different priority levels, You can use the asynq.Queue() option  to send them to different queues.
-		asynq.Queue(worker.QueueCritical),
-	}
-
-	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to distribute task to send verify email: %s", err)
-	}
-
 	rsp := &pb.CreateUserResponse{
-		User: convertUser(user),
+		User: convertUser(txResult.User),
 	}
 	return rsp, nil
 }
